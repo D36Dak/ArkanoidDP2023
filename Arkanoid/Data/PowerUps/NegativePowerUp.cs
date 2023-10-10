5b10cdc66dfe80fc4c4983fdc6315a6d54536d5d
Ignas Rupeika <ignrup2@ktu.lt>
2023-10-10 09:06:50 +0300

    Implemented Abstract Factory for power ups

---
+++ Arkanoid/Data/PowerUps/NegativePowerUp.cs
@@ -0,0 +1,9 @@
+﻿namespace Arkanoid.Data.PowerUps
+{
+    public abstract class NegativePowerUp : PowerUp
+    {
+        public NegativePowerUp(int X, int Y) : base(X, Y, "red", 30)
+        {
+        }
+    }
+}
---
+++ Arkanoid/Data/PowerUps/PositivePowerUpFactory.cs
@@ -0,0 +1,25 @@
+﻿namespace Arkanoid.Data.PowerUps
+{
+    public class PositivePowerUpFactory: PowerUpFactory
+    {
+        public PositivePowerUpFactory()
+        {
+            base.PowerUpTypes.Add("expand");
+            base.PowerUpTypes.Add("piercingBall");
+        }
+
+        public override PowerUp CreatePowerUp(string type, int x, int y)
+        {
+            switch (type)
+            {
+                case "expand":
+                    return new Expand(x, y);
+                case "piercingBall":
+                    return new PiercingBall(x, y);
+                default:
+                    break;
+            }
+            return null;
+        }
+    }
+}
