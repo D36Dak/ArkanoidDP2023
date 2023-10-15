c8eee276ad6d89e9b6bf23c740b1d57a5bcb5656
D36Dak <100153138+D36Dak@users.noreply.github.com>
2023-10-15 16:19:46 +0300

    Revert "Power up factory"

--- Arkanoid/Data/PowerUps/NegativePowerUp.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Arkanoid.Data.PowerUps
-{
-    public abstract class NegativePowerUp : PowerUp
-    {
-        public NegativePowerUp(int X, int Y) : base(X, Y, "red", 30)
-        {
-        }
-    }
-}
--- Arkanoid/Data/PowerUps/PowerUpFactory.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Arkanoid.Data.PowerUps
-{
-    public abstract class PowerUpFactory
-    {
-        public List<string> PowerUpTypes = new List<string>();
-        public abstract PowerUp CreatePowerUp(string type, int x, int y);
-    }
-}
