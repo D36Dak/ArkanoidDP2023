2dc11cd9ec6dab450ac9067fcbc2380e4cbdb78a
Airidas Strelciunas <airstr@ktu.lt>
2023-10-16 13:05:52 +0300

    Builder initial

---
+++ Arkanoid/Data/PowerUps/ConcreteNegativePowerUp.cs
@@ -0,0 +1,10 @@
+﻿namespace Arkanoid.Data.PowerUps
+{
+    public class ConcreteNegativePowerUp: NegativePowerUp
+    {
+        public ConcreteNegativePowerUp(int x, int y) : base(x, y)
+        {
+        }
+    }
+   
+}
---
+++ Arkanoid/Data/PowerUps/NegativePowerUpBuilder.cs
@@ -0,0 +1,31 @@
+﻿using Arkanoid.Data.PowerUps.Arkanoid.Data.PowerUps;
+
+namespace Arkanoid.Data.PowerUps
+{
+    public class NegativePowerUpBuilder : IPowerUpBuilder
+    {
+        private int x, y, size;
+        private string color;
+
+        public void SetPosition(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public void SetColor(string color)
+        {
+            this.color = color;
+        }
+
+        public void SetSize(int size)
+        {
+            this.size = size;
+        }
+
+        public PowerUp Build()
+        {
+            return new ConcreteNegativePowerUp(x, y);
+        }
+    }
+}
