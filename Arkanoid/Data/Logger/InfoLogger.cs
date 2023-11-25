6cc5331386102f962833bb2b78b115504c1ecdd7
Zeptosec <armandasmatkevicius@gmail.com>
2023-11-25 13:04:00 +0200

    Added chain pattern

---
+++ Arkanoid/Data/Logger/InfoLogger.cs
@@ -0,0 +1,21 @@
+﻿namespace Arkanoid.Data.Logger
+{
+    public class InfoLogger : Logger
+    {
+        public InfoLogger(Logger? nextLogger = null) {
+            next = nextLogger;
+        }
+
+        public override void Log(string message, LogLevel level)
+        {
+            if(level == LogLevel.INFO)
+            {
+                ConsoleColor prevCol = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.Write("INFO");
+                Console.ForegroundColor = prevCol;
+                Console.WriteLine($": {message}");
+            } else next?.Log(message, level);
+        }
+    }
+}
