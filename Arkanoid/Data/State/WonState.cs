c7c8d78ff4851b9bfbdf1fa8700ed2077aada489
D36Dak <100153138+D36Dak@users.noreply.github.com>
2023-11-27 20:57:30 +0200

    implemented state

---
+++ Arkanoid/Data/State/WonState.cs
@@ -0,0 +1,16 @@
+﻿namespace Arkanoid.Data.State
+{
+    public class WonState : IGameState
+    {
+        public void Action()
+        {
+            GameEngine ge = GameEngine.GetInstance();
+            _ = ge.StopTimer();
+        }
+
+        public string GetMessage()
+        {
+            return "You Win!";
+        }
+    }
+}
